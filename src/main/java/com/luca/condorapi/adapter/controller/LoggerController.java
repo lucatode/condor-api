2d23993b860b7238f1aeae2bd685bd2aa3110545
Luca Todeschini <luca.todeschini@lastminute.com>
2018-10-20 12:30:03 +0200

    adding first controller

--- src/main/java/com/luca/condorapi/adapter/controller/LoggerController.java
+++ src/main/java/com/luca/condorapi/adapter/controller/LoggerController.java
@@ -1,4 +1,24 @@
 package com.luca.condorapi.adapter.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class LoggerController {
+
+  @GetMapping("/getLogs")
+  public String greeting() {return "";}
+
+  @PutMapping("/info")
+  public ResponseEntity<Void> info(){ return null; }
+
+  @PutMapping("/warn")
+  public ResponseEntity<Void> warn(){ return null; }
+
+  @PutMapping("/error")
+  public ResponseEntity<Void> error(){ return null; }
+
 }
--- src/test/java/com/luca/condorapi/adapter/controller/LoggerControllerTest.java
+++ src/test/java/com/luca/condorapi/adapter/controller/LoggerControllerTest.java
@@ -1,7 +1,46 @@
 package com.luca.condorapi.adapter.controller;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
 import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class LoggerControllerTest {
+
+  private MockMvc mvc;
+
+  @Before
+  public void setUp(){
+  }
+
+  @Test
+  public void shouldRetrieveLogs(){
+
+  }
+
+  @Test
+  public void acceeptInfoPut() throws Exception {
+    mvc.perform(put("/info")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(""))
+            .andExpect(status().isAccepted());
+
+    //assertRepoContains(infoLog);
+  }
+
+  @Test
+  public void acceeptWarnPut() throws Exception {
+    mvc.perform(put("/warn")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(""))
+            .andExpect(status().isAccepted());
+
+    //assertRepoContains(infoLog);
+  }
+
 
 }
